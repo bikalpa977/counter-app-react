192aedeaab9125872ae42f4de66b586d95126825
bikalpa977 <gyawali.bikalpa@gmail.com>
2022-09-14 10:50:11 +0545

    composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,39 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+    // imageUrl: "",
+  };
+
+  handleIncrement = (product) => {
+    //console.log("Increment clicked", this);
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
-    //  React.createElement("div");
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={(product) => this.handleIncrement(product)}
+          className="btn btn-secondary btn sm"
+        >
+          Increment
+        </button>
+        {/* <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul> */}
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import Counters from './components/counters';
 
-ReactDOM.render(<Counter />,document.getElementById('root'));
+ReactDOM.render(<Counters />,document.getElementById('root'));
 
 
 
